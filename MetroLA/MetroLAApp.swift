12a830e8fb5bc1fc05ed74496bf51606290ab915
Francis Breidenbach <fbreidenbach@gmail.com>
2021-12-13 13:13:11 -0800

    Initial commit

---
+++ MetroLA/MetroLAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroLAApp.swift
+//  MetroLA
+//
+//  Created by Francis Breidenbach on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MetroLAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ MetroLA/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  MetroLA
+//
+//  Created by Francis Breidenbach on 12/13/21.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
